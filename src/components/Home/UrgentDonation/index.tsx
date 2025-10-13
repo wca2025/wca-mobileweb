9e29010a2e4a822a9f85141adbc02e1cb4f84426
wca1 <wca20251@gmail.com>
2025-10-13 06:26:59 +0000

    v18

--- src/components/Home/UrgentDonation/index.tsx
+++ src/components/Home/UrgentDonation/index.tsx
@@ -7,12 +7,12 @@ import { useContext } from "react";
 const UrgentDonation = () => {
   const donationInfo = useContext(DonationFormContext);
   return (
-    <section className=" bg-[url('/images/background/donate-banner.jpg')] bg-cover sm:py-52 lg:py-28 py-16 bg-no-repeat">
-      <div className="container mx-auto lg:max-w-(--breakpoint-xl) px-4">
-      
-        <h3 className="text-white sm:text-2xl text-lg font-bold mb-5">
-      
-        </h3>
+    <section className="relative bg-[url('/images/background/donate-banner.jpg')] bg-cover sm:py-52 lg:py-28 py-16 bg-no-repeat">
+      {/* Overlay to darken the background */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      <div className="relative container mx-auto lg:max-w-[--breakpoint-xl] px-4">
+        <h3 className="text-white sm:text-2xl text-lg font-bold mb-5"> </h3>
       </div>
     </section>
   );
